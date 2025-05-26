5c7190137f75fec1eafde62245e3431af0f6fc9a
ShadowMP252 <shadowmp999@gmail.com>
2025-05-26 00:43:06 -0400

    Addd out directory for complied java files...also modified test.java to complile.

---
+++ tests/java/test.java
@@ -0,0 +1,7 @@
+public class test{
+    public static void main(String[] args) {
+        System.out.println("Testing Java Compiler..");
+        System.out.println("Adding Out Directory...");
+        System.out.println("Testing Out Folder!..");
+    }
+}
